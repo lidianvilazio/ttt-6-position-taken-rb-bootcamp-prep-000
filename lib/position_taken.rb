bd4c6c030ee1683d097338b074d70f29676d98ae
Lidian Vilazio <lidianvilazio@hotmail.com>
2018-01-21 23:55:32 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
-position_taken?(board, position)
-  if board[position] == " " board[position] || == ""
+position_taken?(board, index)
+  if board[index] == " " board[index] || == ""
     return false
   end
   return true
